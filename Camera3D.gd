40db574af14db478bd1232e1cd65f433b1c4c392
Thomas COUTAYE <thomascoutaye97434@gmail.com>
2024-03-30 14:32:20 +0100

    character camera first person + movement

--- Camera3D.gd
+++ Camera3D.gd
@@ -1,14 +1,21 @@
 extends Camera3D
 
-@onready var character = get_parent()
-
-
+@onready var character : CharacterBody3D = get_parent()
+var mouse_sens : float = 0.3
+var camera_anglev : float = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.y = character.position.y
 	position.x = character.position.x
 
+func _input(event : InputEvent):
+	if event is InputEventMouseMotion :
+		var changev=-event.relative.y*mouse_sens
+		if camera_anglev+changev>-50 and camera_anglev+changev<50:
+			camera_anglev+=changev
+			rotate_x(deg_to_rad(changev))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
--- character.gd
+++ character.gd
@@ -1,19 +1,23 @@
 extends CharacterBody3D
 
-
-const SPEED = 100
+var camera
+const SPEED = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass 
 
+func _input(event : InputEvent):
+	if (event is InputEventMouseMotion):
+		rotate_y(deg_to_rad(-event.relative.x*$Camera3D.mouse_sens))
+
 func _process(delta) -> void:
-	var dirh : int = Input.get_axis("left","right")
-	var dirv : int = Input.get_axis("up","down")
-	print(dirh)
-	if (dirh != 0):
-		position.x += dirh*10*delta
-	if (dirv != 0):
-		position.z += dirv*10*delta
-	move_and_slide()
 	
+	print(rotation_degrees) 
+	#var camera_pos = Vector2
+	var input_dir : Vector2 = Input.get_vector("left", "right", "up", "down")
+	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		position.x += direction.x * SPEED * delta
+		position.z += direction.z * SPEED * delta
+	move_and_slide()
