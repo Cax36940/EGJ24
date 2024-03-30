606ad9f367c56e8d4dd1c5df7ff322388717ddf7
Thomas COUTAYE <thomascoutaye97434@gmail.com>
2024-03-30 15:11:34 +0100

    full rotation

--- Camera3D.gd
+++ Camera3D.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 
 @onready var character : CharacterBody3D = get_parent()
-var mouse_sens : float = 0.3
+var mouse_sens : float = 0.15
 var camera_anglev : float = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +9,7 @@ func _ready():
 	position.x = character.position.x
 
 func _input(event : InputEvent):
+	print(Input.get_last_mouse_velocity())
 	if event is InputEventMouseMotion :
 		var changev=-event.relative.y*mouse_sens
 		if camera_anglev+changev>-50 and camera_anglev+changev<50:
--- character.gd
+++ character.gd
@@ -1,21 +1,29 @@
 extends CharacterBody3D
 
-var camera
-const MOVEMENT_SPEED = 5
+@onready var camera = $Camera3D
+const MOVEMENT_SPEED = 4
 const FALL_SPEED = 981
+const MAX_VELOCITY_MOUSE = 25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event : InputEvent):
+	
 	if (event is InputEventMouseMotion):
-		rotate_y(deg_to_rad(-event.relative.x*$Camera3D.mouse_sens))
+		var sensitivity = 0.005
+		var mouse_velocity = event.relative.x
+		#var mouse_velocity = event.velocity.x
+		#if (abs(mouse_velocity) > MAX_VELOCITY_MOUSE):
+			#if (mouse_velocity > 0):
+				#mouse_velocity = MAX_VELOCITY_MOUSE
+			#else: 
+				#mouse_velocity = -MAX_VELOCITY_MOUSE
+		print(event.relative.x-event.velocity.x)
+		rotate_y(deg_to_rad(-mouse_velocity * camera.mouse_sens))
 
 func _process(delta) -> void:
-	
-	print(position) 
-	#var camera_pos = Vector2
 	var input_dir : Vector2 = Input.get_vector("left", "right", "up", "down")
 	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
