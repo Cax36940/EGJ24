20690b2dc2e66388d07a55cb376281dd349a7a0e
Thomas COUTAYE <thomascoutaye97434@gmail.com>
2024-03-30 14:39:40 +0100

    Gravity

--- character.gd
+++ character.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
 var camera
-const SPEED = 5
+const MOVEMENT_SPEED = 5
+const FALL_SPEED = 981
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,11 +14,12 @@ func _input(event : InputEvent):
 
 func _process(delta) -> void:
 	
-	print(rotation_degrees) 
+	print(position) 
 	#var camera_pos = Vector2
 	var input_dir : Vector2 = Input.get_vector("left", "right", "up", "down")
 	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		position.x += direction.x * SPEED * delta
-		position.z += direction.z * SPEED * delta
+		position.x += direction.x * MOVEMENT_SPEED * delta
+		position.z += direction.z * MOVEMENT_SPEED * delta
+	velocity.y -= FALL_SPEED * delta
 	move_and_slide()
