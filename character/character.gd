e1c3e033c30f63520bfe6e54d1bd39ad0856ad90
Thomas COUTAYE <thomascoutaye97434@gmail.com>
2024-03-30 17:15:32 +0100

    pause added

--- character/character.gd
+++ character/character.gd
@@ -13,7 +13,6 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event : InputEvent):
-	
 	if (event is InputEventMouseMotion):
 		var sensitivity = 0.005
 		var mouse_velocity = event.relative.x
@@ -22,17 +21,25 @@ func _input(event : InputEvent):
 func _process(delta) -> void:
 	var input_dir : Vector2 = Input.get_vector("left", "right", "up", "down")
 	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var pause 
-	if direction:
-		position.x += direction.x * MOVEMENT_SPEED * delta
-		position.z += direction.z * MOVEMENT_SPEED * delta
-	velocity.y -= FALL_SPEED * delta
-	
-	if (ray_cast.is_colliding()):
-		prev_collider = collider
-		collider = ray_cast.get_collider()
-		print(collider)
-	
-	
+	var pause : bool = false
+	if !(pause):
+		if direction:
+			position.x += direction.x * MOVEMENT_SPEED * delta
+			position.z += direction.z * MOVEMENT_SPEED * delta
+		velocity.y -= FALL_SPEED * delta
+		
+		if (ray_cast.is_colliding()):
+			prev_collider = collider
+			collider = ray_cast.get_collider()
+		else :
+			collider = null
+	if (Input.is_action_just_pressed("interact") and collider != null):
+		if (pause == false):
+			collider.pick_up()
+			pause = true
+		else :
+			collider.put_down()
+			pause = false
 	move_and_slide()
 
+#func raycast() -> Array[]
